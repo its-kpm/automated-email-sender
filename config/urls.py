d6a5f0803c24041bdc61c50d34db28ce2c93e465
KPM <67306285+its-kpm@users.noreply.github.com>
2026-09-12 15:50:48 +0530

    Register authentication routes

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/auth/", include("emails.auth_urls")),
     path("api/emails/", include("emails.urls")),
 ]
