b007b478ba514a592cc418dba667f60b386ec8e2
KPM <67306285+its-kpm@users.noreply.github.com>
2026-09-12 15:52:37 +0530

    Add ownership to email templates

--- emails/models.py
+++ emails/models.py
@@ -1,7 +1,13 @@
+from django.conf import settings
 from django.db import models
 
 
 class EmailTemplate(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="email_templates",
+    )
     name = models.CharField(max_length=100)
     subject = models.CharField(max_length=255)
     body = models.TextField()
